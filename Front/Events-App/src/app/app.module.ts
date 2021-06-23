a890f252cfde6e6e96c26012963abe06dea2cae8
Marcelo Nascimento <marcelo.laravel@gmail.com>
2021-06-23 09:57:24 -0300

    creating nav bar component

--- Front/Events-App/src/app/app.module.ts
+++ Front/Events-App/src/app/app.module.ts
@@ -6,17 +6,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './pages/events/events.component';
 import { SpeakersComponent } from './pages/speakers/speakers.component';
+import { NavComponent } from './layout/nav/nav.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventsComponent,
-    SpeakersComponent
+    SpeakersComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    CollapseModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
